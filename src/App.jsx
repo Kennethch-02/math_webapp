014e3a2b0582a52174a567fba5c53be2b6c626b9
Kenneth C <kennethalonsoc@gmail.com>
2024-08-16 15:17:55 -0600

    refactor: Update App.jsx to use ThemeProvider and ThemeSelector components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Calculator from './components/Calculator'
+import { ThemeProvider, useTheme } from './components/ThemeContext'
+import ThemeSelector from './ThemeSelector'
+
+function AppContent() {
+  const { theme } = useTheme();
+
+  return (
+    <div className={`min-h-screen flex items-center justify-center p-4 ${theme}`}>
+      <ThemeSelector />
+      <Calculator />
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-black to-purple-900 flex items-center justify-center p-4">
-      <Calculator />
-    </div>
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
   )
 }
 
