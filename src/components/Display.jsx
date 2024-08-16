a6be029bfc8e1aba02889bba24e877dc83390795
Kenneth C <kennethalonsoc@gmail.com>
2024-08-16 15:17:50 -0600

    chore: Update Display component styling

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Display({ value }) {
   return (
-    <div className="bg-gray-800 text-right p-4 text-3xl text-purple-400 font-mono rounded mb-4 overflow-hidden glow-border glow-text">
+    <div className="display text-right p-4 text-3xl font-mono rounded mb-4 overflow-hidden">
       {value}
     </div>
   )
